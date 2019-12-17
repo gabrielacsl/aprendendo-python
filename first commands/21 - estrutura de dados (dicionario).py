907be082a98171cfc52282ac08b173766680d035
gabrielacsl <gabrielacsl@users.noreply.github.com>
2019-12-17 02:27:29 -0300

    update

---
+++ first commands/21 - estrutura de dados (dicionario).py
@@ -0,0 +1,5 @@
+x = {'nome': 'Gabriela', 'idade': 18, 'telefone': '12997519772'}
+print(x['telefone'])
+print(len(x)) #quantidade de elementos
+x.pop('idade') #exclui o elemento da lista
+print(x)
---
+++ first commands/22 - estrutura de dados (dicionario 1).py
@@ -0,0 +1,3 @@
+cadastro = [{'nome': 'Gabriela', 'idade': 18}, {'qualquer coisa': 'hm', 'xablau': 'xablau'}]
+print(cadastro[0])
+print(len(cadastro))
