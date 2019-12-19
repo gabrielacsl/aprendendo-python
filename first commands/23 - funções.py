8712d49b5719893e0678580718edf2030e49424c
gabrielacsl <gabrielacsl@users.noreply.github.com>
2019-12-19 14:32:25 -0300

    update

---
+++ first commands/23 - funções.py
@@ -0,0 +1,11 @@
+def MostrarNaTela():
+    print('Hello world!')
+MostrarNaTela()
+
+def SomaNumeros(n1, n2):
+    print('Soma = {}'.format(n1 + n2))
+SomaNumeros(10, 20)
+
+def RetornaMaior(*list):
+    print(max(list))
+RetornaMaior(1, 2, 3, 4, 50, 90, 30)
